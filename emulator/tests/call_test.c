ceedfe43908bc6712dc865a294bd35024f18a2b6
Stephen Lane-Walsh <sdl.slane@gmail.com>
2020-02-16 11:20:47 -0500

    Separate out flag tests and positive/negative tests

--- emulator/tests/call_test.c
+++ emulator/tests/call_test.c
@@ -8,80 +8,88 @@ void setup()
 {
     CPUTicks = 0;
     memset(&R, sizeof(R), 0);
+    R.PC = RAM_OFFSET;
     R.SP = RAM_OFFSET + 0x0100;
 }
 
 UNIT_TEST(CALL_uu)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     _CALL_uu();
     unit_assert_hex_eq(0x1234, R.PC);
     unit_assert_int_eq(20, CPUTicks);
 }
 
-UNIT_TEST(CALL_NZ_uu)
+UNIT_TEST(CALL_NZ_uu_true)
 {
-    R.PC = RAM_OFFSET;
+    writeWord(R.PC, 0x1234);
     R.FZ = true;
     _CALL_NZ_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(CALL_NZ_uu_false)
+{
     writeWord(R.PC, 0x1234);
     R.FZ = false;
     _CALL_NZ_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(28, CPUTicks);
+    unit_assert_int_eq(20, CPUTicks);
 }
 
-UNIT_TEST(CALL_Z_uu)
+UNIT_TEST(CALL_Z_uu_false)
 {
-    R.PC = RAM_OFFSET;
+    writeWord(R.PC, 0x1234);
     R.FZ = false;
     _CALL_Z_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(CALL_Z_uu_true)
+{
     writeWord(R.PC, 0x1234);
     R.FZ = true;
     _CALL_Z_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(28, CPUTicks);
+    unit_assert_int_eq(20, CPUTicks);
 }
 
-UNIT_TEST(CALL_NC_uu)
+UNIT_TEST(CALL_NC_uu_true)
 {
-    R.PC = RAM_OFFSET;
+    writeWord(R.PC, 0x1234);
     R.FC = true;
     _CALL_NC_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(CALL_NC_uu_false)
+{
     writeWord(R.PC, 0x1234);
     R.FC = false;
     _CALL_NC_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(28, CPUTicks);
+    unit_assert_int_eq(20, CPUTicks);
 }
 
-UNIT_TEST(CALL_C_uu)
+UNIT_TEST(CALL_C_uu_false)
 {
-    R.PC = RAM_OFFSET;
+    writeWord(R.PC, 0x1234);
     R.FC = false;
     _CALL_C_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(CALL_C_uu_true)
+{
     writeWord(R.PC, 0x1234);
     R.FC = true;
     _CALL_C_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(28, CPUTicks);
+    unit_assert_int_eq(20, CPUTicks);
 }
 
 UNIT_TEST_SUITE(CALL)
@@ -89,10 +97,14 @@ UNIT_TEST_SUITE(CALL)
 	UNIT_SUITE_SETUP(&setup);
 
 	UNIT_RUN_TEST(CALL_uu);
-	UNIT_RUN_TEST(CALL_NZ_uu);
-	UNIT_RUN_TEST(CALL_Z_uu);
-	UNIT_RUN_TEST(CALL_NC_uu);
-	UNIT_RUN_TEST(CALL_C_uu);
+	UNIT_RUN_TEST(CALL_NZ_uu_true);
+	UNIT_RUN_TEST(CALL_NZ_uu_false);
+	UNIT_RUN_TEST(CALL_Z_uu_false);
+	UNIT_RUN_TEST(CALL_Z_uu_true);
+	UNIT_RUN_TEST(CALL_NC_uu_true);
+	UNIT_RUN_TEST(CALL_NC_uu_false);
+	UNIT_RUN_TEST(CALL_C_uu_false);
+	UNIT_RUN_TEST(CALL_C_uu_true);
 }
 
 int main(int argc, char ** argv)
--- emulator/tests/jump_test.c
+++ emulator/tests/jump_test.c
@@ -8,88 +8,91 @@ void setup()
 {
     CPUTicks = 0;
     memset(&R, sizeof(R), 0);
+    R.PC = RAM_OFFSET;
 }
 
 UNIT_TEST(JP_uu)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     _JP_uu();
     unit_assert_hex_eq(0x1234, R.PC);
     unit_assert_int_eq(12, CPUTicks);
 }
 
-UNIT_TEST(JP_NZ_uu)
+UNIT_TEST(JP_NZ_uu_true)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     R.FZ = true;
     _JP_NZ_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JP_NZ_uu_false)
+{
     writeWord(R.PC, 0x1234);
     R.FZ = false;
     _JP_NZ_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(20, CPUTicks);
+    unit_assert_int_eq(12, CPUTicks);
 }
 
-UNIT_TEST(JP_Z_uu)
+UNIT_TEST(JP_Z_uu_false)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     R.FZ = false;
     _JP_Z_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JP_Z_uu_true)
+{
     writeWord(R.PC, 0x1234);
     R.FZ = true;
     _JP_Z_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(20, CPUTicks);
+    unit_assert_int_eq(12, CPUTicks);
 }
 
-UNIT_TEST(JP_NC_uu)
+UNIT_TEST(JP_NC_uu_true)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     R.FC = true;
     _JP_NC_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JP_NC_uu_false)
+{
     writeWord(R.PC, 0x1234);
     R.FC = false;
     _JP_NC_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(20, CPUTicks);
+    unit_assert_int_eq(12, CPUTicks);
 }
 
-UNIT_TEST(JP_C_uu)
+UNIT_TEST(JP_C_uu_false)
 {
-    R.PC = RAM_OFFSET;
     writeWord(R.PC, 0x1234);
     R.FC = false;
     _JP_C_uu();
     unit_assert_hex_eq(RAM_OFFSET + 2, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JP_C_uu_true)
+{
     writeWord(R.PC, 0x1234);
     R.FC = true;
     _JP_C_uu();
     unit_assert_hex_eq(0x1234, R.PC);
-    unit_assert_int_eq(20, CPUTicks);
+    unit_assert_int_eq(12, CPUTicks);
 }
 
 UNIT_TEST(JP_pHL)
 {
-    R.PC = RAM_OFFSET;
     R.HL = RAM_OFFSET;
     writeWord(R.HL, 0x1234);
     _JP_pHL();
@@ -102,154 +105,200 @@ UNIT_TEST_SUITE(JP)
 	UNIT_SUITE_SETUP(&setup);
 
 	UNIT_RUN_TEST(JP_uu);
-	UNIT_RUN_TEST(JP_NZ_uu);
-	UNIT_RUN_TEST(JP_Z_uu);
-	UNIT_RUN_TEST(JP_NC_uu);
-	UNIT_RUN_TEST(JP_C_uu);
+	UNIT_RUN_TEST(JP_NZ_uu_true);
+	UNIT_RUN_TEST(JP_NZ_uu_false);
+	UNIT_RUN_TEST(JP_Z_uu_false);
+	UNIT_RUN_TEST(JP_Z_uu_true);
+	UNIT_RUN_TEST(JP_NC_uu_true);
+	UNIT_RUN_TEST(JP_NC_uu_false);
+	UNIT_RUN_TEST(JP_C_uu_false);
+	UNIT_RUN_TEST(JP_C_uu_true);
 
 	UNIT_RUN_TEST(JP_pHL);
 }
 
-UNIT_TEST(JR_s)
+UNIT_TEST(JR_s_positive)
 {
-    R.PC = RAM_OFFSET;
     writeByte(R.PC, 2);
     _JR_s();
     unit_assert_hex_eq(RAM_OFFSET + 3, R.PC);
     unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_s_negative)
+{
     writeByte(R.PC, -2);
     _JR_s();
     unit_assert_hex_eq(RAM_OFFSET - 1, R.PC);
-    unit_assert_int_eq(16, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
 }
 
-UNIT_TEST(JR_NZ_s)
+UNIT_TEST(JR_NZ_s_true_positive)
 {
-    R.PC = RAM_OFFSET;
+    writeByte(R.PC, 2);
     R.FZ = true;
     _JR_NZ_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
     unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NZ_s_false_positive)
+{
     writeByte(R.PC, 2);
     R.FZ = false;
     _JR_NZ_s();
     unit_assert_hex_eq(RAM_OFFSET + 3, R.PC);
-    unit_assert_int_eq(12, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NZ_s_true_negative)
+{
+    writeByte(R.PC, -2);
     R.FZ = true;
     _JR_NZ_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
-    unit_assert_int_eq(16, CPUTicks);
+    unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NZ_s_false_negative)
+{
     writeByte(R.PC, -2);
     R.FZ = false;
     _JR_NZ_s();
     unit_assert_hex_eq(RAM_OFFSET - 1, R.PC);
-    unit_assert_int_eq(24, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
 }
 
-UNIT_TEST(JR_Z_s)
+UNIT_TEST(JR_Z_s_false_positive)
 {
-    R.PC = RAM_OFFSET;
+    writeByte(R.PC, 2);
     R.FZ = false;
     _JR_Z_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
     unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_Z_s_true_positive)
+{
     writeByte(R.PC, 2);
     R.FZ = true;
     _JR_Z_s();
     unit_assert_hex_eq(RAM_OFFSET + 3, R.PC);
-    unit_assert_int_eq(12, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_Z_s_false_negative)
+{
+    writeByte(R.PC, -2);
     R.FZ = false;
     _JR_Z_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
-    unit_assert_int_eq(16, CPUTicks);
+    unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_Z_s_true_negative)
+{
     writeByte(R.PC, -2);
     R.FZ = true;
     _JR_Z_s();
     unit_assert_hex_eq(RAM_OFFSET - 1, R.PC);
-    unit_assert_int_eq(24, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
 }
 
-UNIT_TEST(JR_NC_s)
+UNIT_TEST(JR_NC_s_true_positive)
 {
-    R.PC = RAM_OFFSET;
+    writeByte(R.PC, 2);
     R.FC = true;
     _JR_NC_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
     unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NC_s_false_positive)
+{
     writeByte(R.PC, 2);
     R.FC = false;
     _JR_NC_s();
     unit_assert_hex_eq(RAM_OFFSET + 3, R.PC);
-    unit_assert_int_eq(12, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NC_s_true_negative)
+{
+    writeByte(R.PC, -2);
     R.FC = true;
     _JR_NC_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
-    unit_assert_int_eq(16, CPUTicks);
+    unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_NC_s_false_negative)
+{
     writeByte(R.PC, -2);
     R.FC = false;
     _JR_NC_s();
     unit_assert_hex_eq(RAM_OFFSET - 1, R.PC);
-    unit_assert_int_eq(24, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
 }
 
-UNIT_TEST(JR_C_s)
+UNIT_TEST(JR_C_s_false_positive)
 {
-    R.PC = RAM_OFFSET;
+    writeByte(R.PC, 2);
     R.FC = false;
     _JR_C_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
     unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_C_s_true_positive)
+{
     writeByte(R.PC, 2);
     R.FC = true;
     _JR_C_s();
     unit_assert_hex_eq(RAM_OFFSET + 3, R.PC);
-    unit_assert_int_eq(12, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_C_s_false_negative)
+{
+    writeByte(R.PC, -2);
     R.FC = false;
     _JR_C_s();
     unit_assert_hex_eq(RAM_OFFSET + 1, R.PC);
-    unit_assert_int_eq(16, CPUTicks);
+    unit_assert_int_eq(4, CPUTicks);
+}
 
-    R.PC = RAM_OFFSET;
+UNIT_TEST(JR_C_s_true_negative)
+{
     writeByte(R.PC, -2);
     R.FC = true;
     _JR_C_s();
     unit_assert_hex_eq(RAM_OFFSET - 1, R.PC);
-    unit_assert_int_eq(24, CPUTicks);
+    unit_assert_int_eq(8, CPUTicks);
 }
 
 UNIT_TEST_SUITE(JR)
 {
 	UNIT_SUITE_SETUP(&setup);
 
-	UNIT_RUN_TEST(JR_s);
-	UNIT_RUN_TEST(JR_NZ_s);
-	UNIT_RUN_TEST(JR_Z_s);
-	UNIT_RUN_TEST(JR_NC_s);
-	UNIT_RUN_TEST(JR_C_s);
+	UNIT_RUN_TEST(JR_s_positive);
+	UNIT_RUN_TEST(JR_s_negative);
+	UNIT_RUN_TEST(JR_NZ_s_true_positive);
+	UNIT_RUN_TEST(JR_NZ_s_false_positive);
+	UNIT_RUN_TEST(JR_NZ_s_true_negative);
+	UNIT_RUN_TEST(JR_NZ_s_false_negative);
+	UNIT_RUN_TEST(JR_Z_s_false_positive);
+	UNIT_RUN_TEST(JR_Z_s_true_positive);
+	UNIT_RUN_TEST(JR_Z_s_false_negative);
+	UNIT_RUN_TEST(JR_Z_s_true_negative);
+	UNIT_RUN_TEST(JR_NC_s_true_positive);
+	UNIT_RUN_TEST(JR_NC_s_false_positive);
+	UNIT_RUN_TEST(JR_NC_s_true_negative);
+	UNIT_RUN_TEST(JR_NC_s_false_negative);
+	UNIT_RUN_TEST(JR_C_s_false_positive);
+	UNIT_RUN_TEST(JR_C_s_true_positive);
+	UNIT_RUN_TEST(JR_C_s_false_negative);
+	UNIT_RUN_TEST(JR_C_s_true_negative);
 }
 
 int main(int argc, char ** argv)
